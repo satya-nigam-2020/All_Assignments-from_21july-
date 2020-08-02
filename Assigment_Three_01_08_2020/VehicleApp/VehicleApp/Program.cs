9b12cfedb01ff1dd6d605e993e8b954da3d889d4
Satya Prakash Nigam <satya.nigam@zensar.com>
2020-08-02 19:04:08 +0530

    add the Assignment No. 3

--- Assigment_Three_01_08_2020/VehicleApp/VehicleApp/Program.cs
+++ Assigment_Three_01_08_2020/VehicleApp/VehicleApp/Program.cs
@@ -12,9 +12,16 @@ namespace VehicleApp
         static void Main(string[] args)
         {
             Vehicle car = new Vehicle("Ferari","White",0,4,0);
+            Vehicle bus = new Vehicle("TATA", "Blue", 2, 4, 2);
             car.Display();
             car.start();
             car.stop();
+            Console.WriteLine();
+            Console.WriteLine();
+            bus.start();
+            bus.Display();
+            bus.stop();
+
             //Vehicle truck=new Vehicle("Do Not KNow",")
 
             Console.ReadLine();
