20ed56f3906d8b4e38418ec87945dab1d6b0575c
Satya Prakash Nigam <satya.nigam@zensar.com>
2020-08-04 22:37:42 +0530

    adding 3 solution

---
+++ Assigment_4_August_2020/Assigment_4_August_2020/LinkedListAssignment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_4_August_2020
+{
+    class LinkedListAssignment
+    {
+        static void Main()
+        {
+            LinkedList<String> myList = new LinkedList<String>();
+            myList.AddLast("quick");
+            myList.AddLast("fox");
+            myList.AddLast("the");
+            myList.AddLast("lazy");
+            Console.WriteLine("Linked list after adding the elements");
+            foreach (string str in myList)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("myList after doing Addfirst..........");
+            myList.AddFirst("allow");
+            foreach (string str in myList)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("myList after doing Addlast..........");
+            myList.AddLast("team");
+
+            foreach (string str in myList)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("myList after doing Addafter..........");
+            var newNode = myList.AddLast("dad");
+            myList.AddAfter(newNode, "popup");
+            foreach (string str in myList)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("myList after doing Addbefore..........");
+            myList.AddBefore(newNode, "ear");
+            foreach (string str in myList)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("myList after doing find..........");
+            LinkedListNode<String> temp = myList.Find("allow");
+            Console.WriteLine(temp.Value);
+            Console.WriteLine("myList after doing findlast..........");
+            LinkedListNode<String> temp2 = myList.FindLast("ear");
+            Console.WriteLine(temp2.Value);
+            Console.Read();
+
+        }
+    }
+}
---
+++ Assigment_4_August_2020/Assigment_4_August_2020/LinkedListDemo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_4_August_2020
+{
+    class LinkedListDemo
+    {
+        static void Main()
+        {
+
+            LinkedList<String> my_list = new LinkedList<String>();
+
+
+            // Using AddLast() method 
+            my_list.AddLast("Satya");
+            my_list.AddLast("Dipanjan");
+            my_list.AddLast("Sobhandip");
+            my_list.AddLast("Tausif");
+            my_list.AddLast("Girish");
+            my_list.AddLast("Vicky");
+
+            Console.WriteLine("-----------------List of Associates--------------------");
+
+            
+            // LinkedList Using foreach loop 
+            foreach (string str in my_list)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("-----------------List of Associates--------------------");
+
+            my_list.Remove(my_list.First);
+
+            foreach (string str in my_list)
+            {
+                Console.WriteLine(str);
+            }
+
+
+            
+            Console.WriteLine("-----------------List of Associates--------------------");
+
+            my_list.RemoveLast();
+
+            foreach (string str in my_list)
+            {
+                Console.WriteLine(str);
+            }
+
+            
+            my_list.Clear();
+            Console.WriteLine("Number of students: {0}",
+                                         my_list.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
