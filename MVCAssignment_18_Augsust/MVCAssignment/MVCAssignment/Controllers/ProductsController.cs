abab12cc53662481cdde71aa8ec83c6801a87c32
Satya Prakash Nigam <satya.nigam@zensar.com>
2020-08-19 21:04:58 +0530

    adding Mvc Assignment

---
+++ MVCAssignment_18_Augsust/MVCAssignment/MVCAssignment/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using MVCAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAssignment.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            var product = GetProductDetails();
+
+            return View(product);
+        }
+
+        public IEnumerable<Product> GetProductDetails()
+        {
+            return new List<Product>
+            {
+                new Product{Product_Id=101, Product_Name="AC", Rate=45000},
+                 new Product{Product_Id=102, Product_Name="Mobile", Rate=38000},
+                  new Product{Product_Id=103, Product_Name="Bike", Rate=94000}
+            };
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            var pro = GetProductDetails().SingleOrDefault(c => c.Product_Id == id);
+            if (pro == null)
+                return HttpNotFound();
+            return View(pro);
+        }
+    }
+}
---
+++ MVCAssignment_18_Augsust/MVCAssignment/MVCAssignment/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment.Models
+{
+    public class Product
+    {
+        public int Product_Id { get; set; }
+        public string Product_Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
