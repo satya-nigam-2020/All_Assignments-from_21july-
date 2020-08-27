2fc1994f9650883032c2c48d84b11c31d03894d7
Satya Prakash Nigam <satya.nigam@zensar.com>
2020-08-27 20:20:31 +0530

    Sign-In And Register is Done

---
+++ BethanyPieShop2/BethanyPieShop2/Models/DbContextClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BethanyPieShop2.Models
+{
+    public class DbContextClass : DbContext
+    {
+        public DbSet<Register> Registers  { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
